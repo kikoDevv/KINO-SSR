933b31c713b1fea4a5f51905cb84d9cb8b18d54f
kikoDevv <kiko.devv@gmail.com>
2025-01-24 00:28:45 +0100

    Landing page is served

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+app.set('view engine', 'pug');
+app.set('views', './views');
+//--------serve static files from public directory-----
+app.use(express.static('public'));
+app.get('/', (req, res) => {
+    res.render('index', { title: 'Kino', message: 'hello kiko' });
+});
+
+app.listen(5080, () => {
+    console.log('Server is running on port 5080');
+});
