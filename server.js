ba61720f62d5541b5a633fae95bdaadedfd1382a
kikoDevv <kiko.devv@gmail.com>
2025-01-24 21:46:21 +0100

    package config

--- server.js
+++ server.js
@@ -5,9 +5,6 @@ app.set('view engine', 'pug');
 app.set('views', './views');
 //--------serve static files from public directory-----
 app.use(express.static('public'));
-// app.get('/', (req, res) => {
-//     res.render('index', { title: 'Kino', message: 'welcome' });
-// });
 app.get('/', async (req, res) => {
     try {
         const response = await fetch('https://plankton-app-xhkom.ondigitalocean.app/api/movies');
@@ -17,15 +14,12 @@ app.get('/', async (req, res) => {
             description: movie.attributes.intro,
             image: movie.attributes.image.url
         }));
-        console.log(movies); // Log the movies to the console
         res.render('index', { title: 'Kino', movies });
     } catch (error) {
         console.error('Error fetching movies:', error);
         res.status(500).send('Error fetching movies');
     }
 });
-
-
 app.listen(5080, () => {
     console.log('Server running on 5080');
 });
