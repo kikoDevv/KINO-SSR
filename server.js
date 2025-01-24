19cbba5cbcf5b57aafb5d2c94a958c9bc460b340
kikoDevv <kiko.devv@gmail.com>
2025-01-24 22:52:36 +0100

    movieInfo page is added with header and footer

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
 app.set('views', './views');
 app.use(express.static('public'));
+
+//--rout to index
 app.get('/', async (req, res) => {
     try {
         const response = await fetch('https://plankton-app-xhkom.ondigitalocean.app/api/movies');
@@ -18,6 +22,10 @@ app.get('/', async (req, res) => {
         res.status(500).send('Error fetching movies');
     }
 });
+//---rout to moviesInfo
+app.get('/moviesInfo.pug', (req, res) => {
+    res.render('moviesInfo.pug');
+  });
 app.listen(5080, () => {
     console.log('Server running on 5080');
 });
